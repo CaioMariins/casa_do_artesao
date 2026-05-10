609f04b0e228380a7add1370c99470ffdc1095bf
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-05-10 19:48:17 -0300

    feat: adiciona view de listagem de artesãos ativos

--- apps/artesaos/views.py
+++ apps/artesaos/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+from .models import Artesao
+
 # Create your views here.
+
+def listar_artesaos(request):
+    artesaos = Artesao.objects.ativos()
+
+    return render(request, 'lista_artesaos.html', {
+        'artesaos': artesaos
+    })
