565461edad9d9fd11745e66df7832ca98d8dd1aa
Gabriel Vieira <gabrielcnt96@gmail.com>
2026-05-10 22:05:57 -0300

    feat: adiciona view de edição de artesão

--- apps/artesaos/views.py
+++ apps/artesaos/views.py
@@ -1,24 +1,34 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import get_object_or_404, redirect, render
+
 from .forms import ArtesaoForm
 from .models import Artesao
 
 # Create your views here.
 
+
 def listar_artesaos(request):
     artesaos = Artesao.objects.ativos()
 
-    return render(request, 'artesaos/lista_artesaos.html', {
-        'artesaos': artesaos
-    })
+    return render(request, "artesaos/lista_artesaos.html", {"artesaos": artesaos})
 
 
 def criar_artesao(request):
     form = ArtesaoForm(request.POST or None)
 
-    if request.method == 'POST' and form.is_valid():
+    if request.method == "POST" and form.is_valid():
         form.save()
-        return redirect('lista_artesaos')
-    
-    return render(request, 'artesaos/form.html', {
-        'form': form
-    })
+        return redirect("lista_artesaos")
+
+    return render(request, "artesaos/form.html", {"form": form})
+
+
+def editar_artesao(request, id):
+    artesao = get_object_or_404(Artesao, id=id)
+
+    form = ArtesaoForm(request.POST or None, instance=artesao)
+
+    if request.method == "POST" and form.is_valid():
+        form.save()
+        return redirect("lista_artesaos")
+
+    return render(request, "artesaos/form.html", {"form": form})
